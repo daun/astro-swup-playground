c72ec968c900e421c4cd97ca6d6e9a97bdda5685
Philipp Daun <post@philippdaun.net>
2024-04-05 16:25:36 +0200

    Autoformat

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -21,169 +21,172 @@ import SwupSlideTheme from '@swup/slide-theme';
 import SwupOverlayTheme from '@swup/overlay-theme';
 
 const swup = new Swup({
-  // native: true,
-  animationSelector: '[class*="page-transition-"]',
-  containers: ['header', 'main'],
-  // native: true,
-  // animateHistoryBrowsing: true,
-  plugins: [
-    new SwupA11yPlugin({
-      contentSelector: 'body',
-      respectReducedMotion: true,
-      autofocus: true,
-      announcements: {
-        'en': {
-          visit: 'Navigated to: {title}',
-          url: 'New page at {url}'
-        },
-        'de': {
-          visit: 'Navigiert zu: {title}',
-          url: 'Neue Seite unter {url}'
-        },
-        'fr': {
-          visit: 'Navigué vers : {title}',
-          url: 'Nouvelle page à {url}'
-        },
-        'es': {
-          visit: '{title} ha cargado',
-          url: 'Nuevo página en {url}'
-        },
-        '*': {
-          visit: '{title}',
-          url: '{url}'
-        }
-      }
-    }),
-    new SwupBodyClassPlugin(),
-    // new SwupDebugPlugin(),
-    new SwupFormsPlugin(),
-    new SwupFragmentPlugin({
-      rules: [
-        {
-          from: '/fragments/:filter?',
-          to: '/fragments/:filter?',
-          containers: ['#fragments-content', '#fragments-nav', '#fragments-heading']
-        }
-      ]
-    }),
-    new SwupHeadPlugin({ persistAssets: true, awaitAssets: true }),
-    // new SwupJSPlugin({
-    //   animations: [
+	// native: true,
+	animationSelector: '[class*="page-transition-"]',
+	containers: ['header', 'main'],
+	// native: true,
+	// animateHistoryBrowsing: true,
+	plugins: [
+		new SwupA11yPlugin({
+			contentSelector: 'body',
+			respectReducedMotion: true,
+			autofocus: true,
+			announcements: {
+				'en': {
+					visit: 'Navigated to: {title}',
+					url: 'New page at {url}'
+				},
+				'de': {
+					visit: 'Navigiert zu: {title}',
+					url: 'Neue Seite unter {url}'
+				},
+				'fr': {
+					visit: 'Navigué vers : {title}',
+					url: 'Nouvelle page à {url}'
+				},
+				'es': {
+					visit: '{title} ha cargado',
+					url: 'Nuevo página en {url}'
+				},
+				'*': {
+					visit: '{title}',
+					url: '{url}'
+				}
+			}
+		}),
+		new SwupBodyClassPlugin(),
+		// new SwupDebugPlugin(),
+		new SwupFormsPlugin(),
+		new SwupFragmentPlugin({
+			rules: [
+				{
+					from: '/fragments/:filter?',
+					to: '/fragments/:filter?',
+					containers: ['#fragments-content', '#fragments-nav', '#fragments-heading']
+				}
+			]
+		}),
+		new SwupHeadPlugin({ persistAssets: true, awaitAssets: true }),
+		// new SwupJSPlugin({
+		//   animations: [
 
-    //     // Web animations API: parallel
-    //     // {
-    //     //   from: '(.*)',
-    //     //   to: '(.*)',
-    //     //   in: async () => {
-    //     //     const next = document.querySelector('main');
-    //     //     const prev = document.querySelector('main + main');
-    //     //     await Promise.all([
-    //     //       next!.animate([{ opacity: 0, transform: 'translateX(100%)' }, {}], 500).finished,
-    //     //       prev!.animate([{}, { opacity: 0, transform: 'translateX(-100%)' }], 500).finished
-    //     //     ]);
-    //     //   }
-    //     // },
+		//     // Web animations API: parallel
+		//     // {
+		//     //   from: '(.*)',
+		//     //   to: '(.*)',
+		//     //   in: async () => {
+		//     //     const next = document.querySelector('main');
+		//     //     const prev = document.querySelector('main + main');
+		//     //     await Promise.all([
+		//     //       next!.animate([{ opacity: 0, transform: 'translateX(100%)' }, {}], 500).finished,
+		//     //       prev!.animate([{}, { opacity: 0, transform: 'translateX(-100%)' }], 500).finished
+		//     //     ]);
+		//     //   }
+		//     // },
 
-    //     // Web animations API: sequential
-    //     // {
-    //     //   from: '(.*)',
-    //     //   to: '(.*)',
-    //     //   out: async () => {
-    //     //     const container = document.querySelector('main');
-    //     //     const { opacity, transform } = getComputedStyle(container);
-    //     //     const duration = 400 * (opacity - 0);
-    //     //     await container!.animate([
-    //     //       { opacity, transform },
-    //     //       { opacity: 0, transform: 'translateY(10px)' }
-    //     //     ], duration).finished;
-    //     //   },
-    //     //   in: async () => {
-    //     //     const container = document.querySelector('main');
-    //     //     const { opacity, transform } = getComputedStyle(container);
-    //     //     await container!.animate([
-    //     //       { opacity: 0, transform: 'translateY(10px)' },
-    //     //       { opacity: 1, transform: 'none' }
-    //     //     ], 400).finished;
-    //     //   }
-    //     // },
+		//     // Web animations API: sequential
+		//     // {
+		//     //   from: '(.*)',
+		//     //   to: '(.*)',
+		//     //   out: async () => {
+		//     //     const container = document.querySelector('main');
+		//     //     const { opacity, transform } = getComputedStyle(container);
+		//     //     const duration = 400 * (opacity - 0);
+		//     //     await container!.animate([
+		//     //       { opacity, transform },
+		//     //       { opacity: 0, transform: 'translateY(10px)' }
+		//     //     ], duration).finished;
+		//     //   },
+		//     //   in: async () => {
+		//     //     const container = document.querySelector('main');
+		//     //     const { opacity, transform } = getComputedStyle(container);
+		//     //     await container!.animate([
+		//     //       { opacity: 0, transform: 'translateY(10px)' },
+		//     //       { opacity: 1, transform: 'none' }
+		//     //     ], 400).finished;
+		//     //   }
+		//     // },
 
-    //     // // GSAP: sequential
-    //     // {
-    //     //   from: '(.*)',
-    //     //   to: '(.*)',
-    //     //   out: (onComplete) => {
+		//     // // GSAP: sequential
+		//     // {
+		//     //   from: '(.*)',
+		//     //   to: '(.*)',
+		//     //   out: (onComplete) => {
 
-    //     //     const container = document.querySelector('main');
-    //     //     // container.style.opacity = 1;
-    //     //     gsap.to(container, { opacity: 0, duration: 0.5, onComplete });
-    //     //     console.log('out', container);
-    //     //   },
-    //     //   in: (onComplete) => {
-    //     //     const container = document.querySelector('main');
-    //     //     container.style.opacity = 0;
-    //     //     gsap.to(container, { opacity: 1, duration: 0.5, onComplete });
-    //     //     console.log('in', container);
-    //     //   }
-    //     // },
-    //   ]
-    // }),
-    // new SwupParallelPlugin({ containers: ['main'], keep: 2 }),
-    // new SwupProgressPlugin(),
-    // new SwupRouteNamePlugin({
-    //   routes: [
-    //     { name: 'home', path: '/' },
-    //     { name: 'filter', path: '/fragments/:slug?' },
-    //     { name: 'sub', path: '/:slug' },
-    //     { name: 'any', path: '(.*)' }
-    //   ]
-    // }),
-    new SwupScriptsPlugin(),
-    // new SwupScrollPlugin({
-    //   animateScroll: {
-    //     betweenPages: false,
-    //     samePageWithHash: true,
-    //     samePage: true
-    //   },
-    //   doScrollingRightAway: false,
-    //   // shouldResetScrollPosition: (e) => false,
-    //   offset: 30,
-    //   markScrollTarget: true
-    // }),
-    new SwupMorphPlugin({ containers: ['#randoms'] }),
-    // new SwupFadeTheme(),
-    // new SwupSlideTheme({ reversed: false }),
-    // new SwupOverlayTheme({ color: '#000', duration: 500, direction: 'to-top' }),
-  ]
-})
+		//     //     const container = document.querySelector('main');
+		//     //     // container.style.opacity = 1;
+		//     //     gsap.to(container, { opacity: 0, duration: 0.5, onComplete });
+		//     //     console.log('out', container);
+		//     //   },
+		//     //   in: (onComplete) => {
+		//     //     const container = document.querySelector('main');
+		//     //     container.style.opacity = 0;
+		//     //     gsap.to(container, { opacity: 1, duration: 0.5, onComplete });
+		//     //     console.log('in', container);
+		//     //   }
+		//     // },
+		//   ]
+		// }),
+		// new SwupParallelPlugin({ containers: ['main'], keep: 2 }),
+		// new SwupProgressPlugin(),
+		// new SwupRouteNamePlugin({
+		//   routes: [
+		//     { name: 'home', path: '/' },
+		//     { name: 'filter', path: '/fragments/:slug?' },
+		//     { name: 'sub', path: '/:slug' },
+		//     { name: 'any', path: '(.*)' }
+		//   ]
+		// }),
+		new SwupScriptsPlugin(),
+		// new SwupScrollPlugin({
+		//   animateScroll: {
+		//     betweenPages: false,
+		//     samePageWithHash: true,
+		//     samePage: true
+		//   },
+		//   doScrollingRightAway: false,
+		//   // shouldResetScrollPosition: (e) => false,
+		//   offset: 30,
+		//   markScrollTarget: true
+		// }),
+		new SwupMorphPlugin({ containers: ['#randoms'] })
+		// new SwupFadeTheme(),
+		// new SwupSlideTheme({ reversed: false }),
+		// new SwupOverlayTheme({ color: '#000', duration: 500, direction: 'to-top' }),
+	]
+});
 
 window.swup = swup;
 
 swup.hooks.on('visit:start', (visit) => {
-  console.log('visit:start', '----------------------------');
-})
+	console.log('visit:start', '----------------------------');
+});
 
 swup.hooks.on('visit:start', (visit) => {
-  if (visit.to.url === '/stress-testing') {
-    console.log('manually aborting visit', visit.to.url );
-    // setTimeout(() => visit.abort(), 5);
-  }
-})
+	if (visit.to.url === '/stress-testing') {
+		console.log('manually aborting visit', visit.to.url);
+		// setTimeout(() => visit.abort(), 5);
+	}
+});
 
-window.addEventListener('swup:any', ({ detail: { hook, visit, args }}) => {
-  const normal = 'color: black; font-weight: normal;';
-  const bold = 'color: black; font-weight: bold;';
-  const blue = 'color: blue; font-weight: bold;';
-  const red = 'color: red; font-weight: bold;';
-  const green = 'color: green; font-weight: bold;';
-  const name = [hook, hook === 'content:replace' ? blue : bold];
-  const url = [visit.to.url, normal];
-  const status = visit.aborted ? ['aborted', red] : ['valid', green]; // ['valid', green]
-  const params = false ? [args] : [];
-  console.log(
-    `%c ${name[0]} %c ${url[0]} %c ${status[0]}`,
-    name[1], url[1], status[1], ...params
-  );
-})
+window.addEventListener('swup:any', ({ detail: { hook, visit, args } }) => {
+	const normal = 'color: black; font-weight: normal;';
+	const bold = 'color: black; font-weight: bold;';
+	const blue = 'color: blue; font-weight: bold;';
+	const red = 'color: red; font-weight: bold;';
+	const green = 'color: green; font-weight: bold;';
+	const name = [hook, hook === 'content:replace' ? blue : bold];
+	const url = [visit.to.url, normal];
+	const status = visit.aborted ? ['aborted', red] : ['valid', green]; // ['valid', green]
+	const params = false ? [args] : [];
+	console.log(
+		`%c ${name[0]} %c ${url[0]} %c ${status[0]}`,
+		name[1],
+		url[1],
+		status[1],
+		...params
+	);
+});
 
 // swup.hooks.replace('fetch:request', function(visit, { url, options }) {
 //   const controller = new AbortController();
